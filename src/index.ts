6d97375e8d7229976af8fec0151a7d5ed1c587d0
BalaAshwanth <balaashwanth.natarajan@ideas2it.com>
2022-12-15 19:10:24 +0530

    another test for it

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,63 @@ import Mustache from "mustache";
 import "./styles.scss";
 
 export class UserDefinedTemplate {
-    constructor(toBeRendered: object) {
+    constructor(toBeRendered: Array<object>) {
         this.initTableCreation(toBeRendered)
     }
 
-    initTableCreation(items: object) {
-        const tableHead = Object.keys(items)
-        const tableBody = Object.values(items)
-        const toRender: object = {}
-        toRender["header"] = tableHead;
-        console.log(tableBody)
-        let maxLength = 0
-        for (const item of tableBody) {
-            if (maxLength < item.length) {
-                maxLength = item.length
+    initTableCreation(items: Array<object>) {
+
+         const toRender: object = {}
+        let allHeaders: Array<string> = [];
+        for (const item of items) {
+            const keyLength = Object.keys(item)
+            if (allHeaders.length < keyLength.length) {
+                allHeaders = Object.keys(item)
             }
         }
 
-        for (let i = 0; i < maxLength; i++) {
-            const data: Array<string> = [];
-            for (const item of tableBody) {
-                if (item[i] != null) {
-                    data.push(item[i])
+        const tableHead: Array<string> = [];
+        for (const key of allHeaders) {
+            tableHead.push(this.capitalize(key))
+        }
+        const sampleData = []
+        for(let i = 0; i < allHeaders.length; i++) {
+            sampleData.push("")
+        }        
+        let count = 0
+        for (const item of items) {
+            count += 1;
+            const data: string[] = JSON.parse(JSON.stringify(sampleData))
+            const headers = Object.keys(item);
+            for(let i = 0; i < allHeaders.length; i++) {
+                const value = headers[i];
+                if (allHeaders.includes(value)) {
+                    const index = allHeaders.indexOf(headers[i]);
+                    data[index] = item[value];
                 }
                 else {
-                    data.push("")
+                    tableHead.push(this.capitalize(value))
+                    data.push(item[value]);
                 }
             }
-            toRender["item-" + (i + 1)] = data
+            toRender[`item-${count}`] = data
         }
+        toRender["header"] = tableHead;
+        console.log(toRender);
 
-        const template = this.getTemplete(maxLength)
+
+        const template = this.getTemplete(allHeaders.length)
 
         const render = Mustache.render(template, toRender)
         document.getElementById("user-defined-table").innerHTML = render;
+    }
 
+
+    capitalize(text: string) {
+        const capital = text.charAt(0).toUpperCase();
+        const remaining = text.substring(1, text.length)
+        const result = capital + remaining;
+        return result;
     }
 
     getTemplete(maxLength: number) {
@@ -69,11 +91,32 @@ export class UserDefinedTemplate {
     }
 }
 
-const a = new UserDefinedTemplate({
-    Name: ["juba", "Viswa", "Bala Ashwanth", "Jagan"],
-    Place: ["Japan", "Veitnam", "Bangladesh", "jamaica"],
-    Things: ["Jet", "Van", "Ball", ""],
-    Animals: ["Jaguar", "Vagar", "bat", "Jagan"]
-})
+
+const bc = [{
+    name: "juba",
+    place: "Japan",
+    things: "Jet",
+    Animals: "Jaguar"
+},
+{
+    name: "Viswa",
+    place: "Veitnam",
+    things: "Van",
+    Animals: "Vagar"
+},
+{
+    name: "Bala Ashwanth",
+    place: "Bangladesh",
+    things: "Ball",
+    Animals: "bat"
+},
+{
+    name: "Jagan",
+    place: "jamaica",
+    age: 21,
+    Animals: "Jagan",
+    
+}]
+const a = new UserDefinedTemplate(bc)
 
 
